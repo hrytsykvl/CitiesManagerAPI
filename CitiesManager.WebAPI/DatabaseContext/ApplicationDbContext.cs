9ba2ec27701fc8d7ed9ed988d3e0ee30f42444fb
hrytsykvl <gricikvlad@gmail.com>
2024-05-25 13:26:37 +0300

    Make database with EF

---
+++ CitiesManager.WebAPI/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using CitiesManager.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitiesManager.WebAPI.DatabaseContext
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public ApplicationDbContext()
+		{
+		}
+
+		public virtual DbSet<City> Cities { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<City>().HasData(new City()
+			{
+				CityID = Guid.Parse("9072C6B2-B43F-41C1-9B3B-BC248C92B457"),
+				CityName = "Kyiv"
+			});
+
+			modelBuilder.Entity<City>().HasData(new City()
+			{
+				CityID = Guid.Parse("F3E6CCEF-006A-482B-BD25-B186C41824C6"),
+				CityName = "Lviv"
+			});
+		}
+	}
+}
---
+++ CitiesManager.WebAPI/Migrations/20240525101858_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CitiesManager.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "CityID", "CityName" },
+                values: new object[,]
+                {
+                    { new Guid("9072c6b2-b43f-41c1-9b3b-bc248c92b457"), "Kyiv" },
+                    { new Guid("f3e6ccef-006a-482b-bd25-b186c41824c6"), "Lviv" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
