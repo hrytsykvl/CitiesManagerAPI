d19e8676d3f6b1aefccad1992ade435f91d5ac6b
hrytsykvl <gricikvlad@gmail.com>
2024-06-16 12:54:57 +0300

    Create login endpoint and UI, logout functionality

--- CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/account.service.ts
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/account.service.ts
@@ -2,17 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/register-user';
 import { Observable } from 'rxjs';
+import { LoginUser } from '../models/login-user';
 
 const API_BASE_URL: string = "https://localhost:7119/api/v1/account/";
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  public currentUserName: string | null = null;
 
   constructor(private httpClient: HttpClient) {
   }
 
   public postRegister(registerUser: RegisterUser): Observable<RegisterUser> {
     return this.httpClient.post<RegisterUser>(`${API_BASE_URL}register`, registerUser);
+  }
+
+  public postLogin(loginUser: LoginUser): Observable<LoginUser> {
+    return this.httpClient.post<LoginUser>(`${API_BASE_URL}login`, loginUser);
+  }
+
+  public getLogout(): Observable<string> {
+    return this.httpClient.get<string>(`${API_BASE_URL}logout`);
   }
 }
--- CitiesManager.WebAPI/Controllers/v1/AccountController.cs
+++ CitiesManager.WebAPI/Controllers/v1/AccountController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CitiesManager.WebAPI.Controllers.v1
 {
+	/// <summary>
+	/// Controller for account actions
+	/// </summary>
 	[AllowAnonymous]
 	[ApiVersion("1.0")]
 	public class AccountController : CustomControllerBase
@@ -15,6 +18,12 @@ namespace CitiesManager.WebAPI.Controllers.v1
 		private readonly SignInManager<ApplicationUser> _signInManager;
 		private readonly RoleManager<ApplicationRole> _roleManager;
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="userManager"></param>
+		/// <param name="signInManager"></param>
+		/// <param name="roleManager"></param>
 		public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
 		{
 			_userManager = userManager;
@@ -22,6 +31,11 @@ namespace CitiesManager.WebAPI.Controllers.v1
 			_roleManager = roleManager;
 		}
 
+		/// <summary>
+		/// Registers account in database
+		/// </summary>
+		/// <param name="registerDTO"></param>
+		/// <returns></returns>
 		[HttpPost("register")]
 		public async Task<ActionResult<ApplicationUser>> PostRegister(RegisterDTO registerDTO)
 		{
@@ -57,6 +71,11 @@ namespace CitiesManager.WebAPI.Controllers.v1
 			}
 		}
 
+		/// <summary>
+		/// Defines if user already registered
+		/// </summary>
+		/// <param name="email"></param>
+		/// <returns></returns>
 		[HttpGet]
 		public async Task<IActionResult> IsEmailAlreadyRegistered(string email)
 		{
@@ -70,6 +89,53 @@ namespace CitiesManager.WebAPI.Controllers.v1
 			{
 				return Ok(false);
 			}
+		}
+
+		/// <summary>
+		/// Log in the user
+		/// </summary>
+		/// <param name="loginDTO"></param>
+		/// <returns></returns>
+		[HttpPost("login")]
+		public async Task<IActionResult> PostLogin(LoginDTO loginDTO)
+		{
+			//Validation
+			if (ModelState.IsValid == false)
+			{
+				string errorMessage = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+				return Problem(errorMessage);
+			}
+
+			var result = await _signInManager.PasswordSignInAsync(loginDTO.Email, loginDTO.Password,
+				isPersistent: false, lockoutOnFailure: false);
+
+			if(result.Succeeded)
+			{
+				ApplicationUser? user = await _userManager.FindByEmailAsync(loginDTO.Email);
+
+				if(user == null )
+				{
+					return NoContent();
+				}
+				return Ok(new { personName = user.PersonName, email = user.Email });
+			}
+			else
+			{
+				return Problem("Invalid email or password");
+			}
+		}
+
+		/// <summary>
+		/// Log out the user
+		/// </summary>
+		/// <param name="loginDTO"></param>
+		/// <returns></returns>
+		[HttpGet("logout")]
+		public async Task<IActionResult> GetLogout(LoginDTO loginDTO)
+		{
+			await _signInManager.SignOutAsync();
+
+			return NoContent();
 		}
 	}
 }
