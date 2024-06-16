7af769a48d3429dabc45f1bff6530a16747b6466
hrytsykvl <gricikvlad@gmail.com>
2024-06-16 11:59:05 +0300

    Create Register endpoint and UI

---
+++ CitiesManager.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CitiesManager.Core.DTO
+{
+	public class RegisterDTO
+	{
+		[Required(ErrorMessage = "Person name can't be blank")]
+		public string PersonName { get; set; }
+
+		[Required(ErrorMessage = "Email can't be blank")]
+		[EmailAddress(ErrorMessage = "Email should be in a proper email address format")]
+		[Remote(action : "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email is alredy being used")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Phone number can't be blank")]
+		[RegularExpression("^[0-9]*$", ErrorMessage = "Phone number should only contain digits")]
+		[Remote(action: "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email is alredy being used")]
+		public string PhoneNumber { get; set; }
+
+		[Required(ErrorMessage = "Password can't be blank")]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Confirm Password can't be blank")]
+		[Compare("Password", ErrorMessage = "Confirm password should match with password")]
+		public string ConfirmPassword { get; set; }
+	}
+}
--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -1,4 +1,7 @@
+using CitiesManager.Core.Identity;
 using CitiesManager.Infrastructure.DatabaseContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
@@ -67,12 +70,28 @@ builder.Services.AddCors(options =>
 	});
 });
 
+//Identity
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+	options.Password.RequiredLength = 5;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequireUppercase = false;
+	options.Password.RequireLowercase = true;
+	options.Password.RequireDigit = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders()
+.AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
+.AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, Guid>>()
+;
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHsts();
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseSwagger();
 app.UseSwaggerUI(options =>
@@ -83,6 +102,7 @@ app.UseSwaggerUI(options =>
 app.UseRouting();
 app.UseCors();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
